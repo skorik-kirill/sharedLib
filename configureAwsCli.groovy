d0b6b8f617058649564bf3095b5e8270deebd64d
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2025-05-12 16:57:16 +0300

    Create configureAwsCli.groovy

---
+++ configureAwsCli.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def configureAwsCli(body) {
+    withCredentials([[
+        $class: 'AmazonWebServicesCredentialsBinding',
+        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY',
+        credentialsId: 'AWSCLICredentials'
+    ]]) {
+        body()
+    }
+}
